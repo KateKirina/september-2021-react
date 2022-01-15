e7fc7062f9cd528a2dff77539c86ab9c19dd54f6
kiriny <info.kate.kirina@gmail.com>
2022-01-15 21:01:28 +0200

    create hw2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import Users from "./components/Users/Users";
+import UserDetails from "./components/UsersDetails/UserDetails";
+import Posts from "./components/Posts/Posts";
+import css from "./App.module.css";
+import {postService} from "./services/post.service";
+
 
 const App = () => {
+    const [user, setUser] = useState(null);
+    const [userId, setUserId] = useState(null);
+    const [posts, setPosts] = useState([]);
+
+    const getUser = (user) => {
+        setUser(user)
+    }
+
+    const getUserId = (id) => {
+        postService.getByUserId(id).then(value => setPosts([...value]))
+    }
+
   return (
       <div>
-        
+            <div className={css.wrap}>
+                <Users getUser={getUser}/>
+                {user && <UserDetails user={user} getUserId={getUserId}/>}
+            </div>
+          {!!posts.length && <Posts posts={posts}/>}
       </div>
   );
 };
